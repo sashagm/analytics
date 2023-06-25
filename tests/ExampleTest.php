3ad85d8c65cc7f65917c843dbf55f9b47fb0c870
Sashagm <example@example.com>
2023-06-25 22:37:44 +0500

    Добавлено тестирование phpunit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Sashagm\Analytics\Tests;
+
+use Sashagm\Analytics\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test  **/
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Sashagm\Analytics\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    //
+}
+
+
+
+
+
+
